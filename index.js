7f96d32bb9e1d8fb7c66b7485f816bb7c4fc4afe
calvin <cbanning@udel.edu>
2020-01-22 16:44:04 -0500

    updates

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//refactor this to be one function that passes in the page name
+
 function showA() {
     document.getElementById("pageA").style.visibility = "visible";
     document.getElementById("pageB").style.visibility = "hidden";
@@ -6,10 +8,6 @@ function showA() {
     document.getElementById("pageA").style.opacity = 1;
     document.getElementById("pageB").style.opacity = 0;
     document.getElementById("pageC").style.opacity = 0;
-
-    document.getElementById("buttonA").style.fontWeight = 'bold';
-    document.getElementById("buttonB").style.fontWeight = 'normal';
-    document.getElementById("buttonC").style.fontWeight = 'normal';
 
     document.getElementById("buttonA").style.color = '#000';
     document.getElementById("buttonB").style.color = '#aaa';
@@ -25,10 +23,6 @@ function showB() {
     document.getElementById("pageC").style.opacity = 0;
     document.getElementById("pageA").style.opacity = 0;
 
-    document.getElementById("buttonB").style.fontWeight = 'bold';
-    document.getElementById("buttonA").style.fontWeight = 'normal';
-    document.getElementById("buttonC").style.fontWeight = 'normal';
-
     document.getElementById("buttonB").style.color = '#000';
     document.getElementById("buttonA").style.color = '#aaa';
     document.getElementById("buttonC").style.color = '#aaa';
@@ -42,10 +36,6 @@ function showC() {
     document.getElementById("pageC").style.opacity = 1;
     document.getElementById("pageB").style.opacity = 0;
     document.getElementById("pageA").style.opacity = 0;
-
-    document.getElementById("buttonC").style.fontWeight = 'bold';
-    document.getElementById("buttonB").style.fontWeight = 'normal';
-    document.getElementById("buttonA").style.fontWeight = 'normal';
 
     document.getElementById("buttonC").style.color = '#000';
     document.getElementById("buttonB").style.color = '#aaa';
